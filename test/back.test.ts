f45329fc24c9d7c638cb31884e758d6f9e492d37
redamaidy <edgar.juareze@elektra.com.mx>
2022-11-22 13:21:52 -0600

    Initial commit

---
+++ test/back.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Back from '../lib/back-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/back-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Back.BackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
