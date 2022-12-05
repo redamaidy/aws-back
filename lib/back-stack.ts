d1426b817dab609a550b72ccc388b1c67d9c1baf
redamaidy <edgar.juareze@elektra.com.mx>
2022-12-05 11:43:26 -0600

    fist commit

--- lib/back-stack.ts
+++ lib/back-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export class BackStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { Code, Function, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Cors, LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, ProjectionType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+export class BackStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    let corsEnvironment = {
+      CORS_ORIGIN: "*"
+    };
 
-    // The code that defines your stack goes here
+    // reusable RESTful API CORS options object
+    let corsOptions = {
+      allowOrigins: [ "*" ], // array containing an origin, or Cors.ALL_ORIGINS
+      allowMethods: Cors.ALL_METHODS, // array of methods eg. [ 'OPTIONS', 'GET', 'POST', 'PUT', 'DELETE' ]
+    };
+    const dynamodbTable = new Table(this, 'dynamodb-table-ejmplo', {
+      partitionKey: { name: 'username', type: AttributeType.STRING },
+      sortKey: { name: 'id', type: AttributeType.NUMBER },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      timeToLiveAttribute: 'expiration',
+      tableName:'tablaXD'
+    });
+    dynamodbTable.addGlobalSecondaryIndex({
+      indexName: 'dynamodb-table-index',
+      projectionType: ProjectionType.KEYS_ONLY,
+      partitionKey: { name: 'id', type: AttributeType.NUMBER },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const first_function = new Function(this, 'first-function', {
+      runtime: Runtime.NODEJS_16_X,
+      handler: 'lambda.handler',
+      code: Code.fromAsset('./src'),
+      environment: {
+        ...corsEnvironment,
+        TABLE_NAME: dynamodbTable.tableName
+      }
+    });
+    dynamodbTable.grantWriteData(first_function);
+    dynamodbTable.grantReadData(first_function);
+    new apigateway.LambdaRestApi(this, 'api-rest', {
+      handler: first_function,
+      defaultCorsPreflightOptions: corsOptions
+    });
   }
 }
